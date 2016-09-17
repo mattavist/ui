fd3953217138bc102d20e20fba73b5f7288413c1
Mattavist <matavist@gmail.com>
2016-09-17 16:21:30 -0700

    Alert Frames handled, but things are messy

--- mFrame/reposition.lua
+++ mFrame/reposition.lua
@@ -1,10 +1,76 @@
 local f = CreateFrame("Frame")
+local s = CreateFrame("Frame")
 
-function f:OnEvent(event, addon)
+local framesToMove = {
+    ["ObjectiveTrackerFrame"] = {
+        name = ObjectiveTrackerFrame,
+        func = "SetPoint",
+        frameAnchor = "TOPLEFT",
+        parent = UIParent,
+        parentAnchor = "TOPLEFT",
+        x = 30,
+        y = -12,
+        height = 500
+    },
+    ["AlertFrame"] = {
+        name = AlertFrame,
+        func = "SetParent",
+        frameAnchor = "TOP",
+        parent = UIParent,
+        parentAnchor = "BOTTOM",
+        x = 0,
+        y = 600,
+        height = nil
+    },
+    ["TalkingHeadFrame"] = {
+        name = TalkingHeadFrame,
+        func = "TalkingHeadFrame_PlayCurrent",
+        frameAnchor = "TOP",
+        parent = WorldFrame,
+        parentAnchor = "CENTER",
+        x = 0,
+        y = -300,
+        height = 300
+    },
+    ["ArcheologyDigsiteProgressBar"] = {
+        name = {ArcheologyDigsiteProgressBar},
+        func = "ArcheologyDigsiteProgressBar_OnUpdate",
+        frameAnchor = "BOTTOM",
+        parent = WorldFrame,
+        parentAnchor = "BOTTOM",
+        x = 0,
+        y = 50,
+        height = nil
+    }
+}
+
+local function moveFrame(frame)
+    if frame.hooked then
+        return
+    end
+    frame.hooked = true
+    local moving
+    hooksecurefunc(frame.name, frame.func, function(self)
+        if moving then
+            return
+        end
+        moving = true
+        self:ClearAllPoints()
+        self:SetPoint(frame.frameAnchor, frame.parent, frame.parentAnchor, frame.x, frame.y)
+        if frame.height then
+            self:SetHeight(frame.height)
+        end
+        moving = nil
+    end)
+end
+
+function s:OnAddon(event, addon)
     -- Move Talking Head Frame to top of screen
     if addon == "Blizzard_TalkingHeadUI" then
         hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
-            TalkingHeadFrame:SetPoint("BOTTOM", WorldFrame, "TOP", 0, -300)
+            AlertFrame:ClearAllPoints()
+            AlertFrame:SetPoint("BOTTOM", 0, 350)
+            TalkingHeadFrame:SetPoint("BOTTOM", WorldFrame, "BOTTOM", 0, 350)
             TalkingHeadFrame.ignoreFramePositionManager = true
         end)
 
@@ -17,5 +83,17 @@ function f:OnEvent(event, addon)
     end
 end
 
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", f.OnEvent)
+function f:OnEnter()
+    moveFrame(framesToMove["ObjectiveTrackerFrame"])
+    --moveFrame(framesToMove["AlertFrame"])
+    AlertFrame:ClearAllPoints()
+    AlertFrame:SetPoint("BOTTOM", 0, 350)
+    f:UnregisterEvent("PLAYER_ENTERING_WORLD")
+    s:SetScript("OnEvent", s.OnAddon)
+    s:RegisterEvent("ADDON_LOADED")
+end
+
+
+f:SetScript("OnEvent", f.OnEnter)
+s:SetScript("OnEvent", s.OnAddon)
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
