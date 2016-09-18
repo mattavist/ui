13233bd1b85c25ba25a8414e52db4c4e7b9c547d
Mattavist <matavist@gmail.com>
2016-09-17 20:02:56 -0700

    Class Colored Names

--- mTooltip/mTooltip.lua
+++ mTooltip/mTooltip.lua
@@ -1,11 +1,33 @@
 local tip = CreateFrame('Frame', UIParent)
 
+local classColors = {
+    ["DEATH KNIGHT"] = "|cFFC41F3B",
+    ["DEMON HUNTER"] = "|cFFA330C9",
+    ["DRUID"] = "|cFFFF7D0A",
+    ["HUNTER"] = "|cFFABD473",
+    ["MAGE"] = "|cFF69CCF0",
+    ["MONK"] = "|cFF00FF96",
+    ["PALADIN"] = "|cFFF58CBA",
+    ["PRIEST"] = "|cFFFFFFFF",
+    ["ROGUE"] = "|cFFFFF569",
+    ["SHAMAN"] = "|cFF0070DE",
+    ["WARLOCK"] = "|cFF9482C9",
+    ["WARRIOR"] = "|cFFC79C6E"
+}
+
 local function line()
+    --Recolor unit name
+    local class, cls = UnitClass("mouseover")
+    if cls then
+        local playerName = _G["GameTooltipTextLeft" .. 1]
+        playerName:SetText(classColors[cls]..playerName:GetText())
+    end
+
     -- Reanchor the tooltip
     GameTooltip:ClearAllPoints()
     GameTooltip:SetPoint("BOTTOMLEFT", WorldFrame, "BOTTOMRIGHT", -700, 400)
 
-    -- Add 
+    -- Add target line
     local target = UnitName("mouseovertarget")
     local player = UnitName("player")
     if target == player then
@@ -14,8 +36,9 @@ local function line()
 
     if target then
         GameTooltip:AddLine(target, 1, 1, 1)
-        GameTooltip:Show()
     end
+
+    GameTooltip:Show()
 end
 
 tip:SetScript("OnEvent", function () line() end)
