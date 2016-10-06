08b11190eb3fc857fac163b16cace56726796f6c
Mattavist <matavist@gmail.com>
2016-10-05 21:40:37 -0700

    Added mChar

---
+++ AddOns/mChar/mChar.lua
@@ -0,0 +1,35 @@
+local charData
+
+local loader = CreateFrame("Frame", nil, UIParent)
+loader:RegisterEvent("PLAYER_ENTERING_WORLD")
+loader:SetScript("OnEvent", function()
+    charData = mCharData
+    if not charData then
+		charData = {}
+	end
+	charData[UnitName("player")] = floor(abs(GetMoney() / 10000))
+	mCharData = charData
+end)
+
+local saver = CreateFrame("Frame", nil, UIParent)
+saver:RegisterEvent("PLAYER_LOGOUT")
+saver:SetScript("OnEvent", function()
+	if charData then
+    	mCharData = charData
+    end
+end)
+
+SlashCmdList["TRY"] = function()
+	local total = 0
+	charData[UnitName("player")] = floor(abs(GetMoney() / 10000))
+	mCharData = charData
+
+	for key, value in pairs(charData) do
+		if value > 30 then
+    		ChatFrame3:AddMessage(key.."  -  "..value.."g")
+    	end
+    	total = total + value
+    end
+    ChatFrame3:AddMessage("Total  -  "..total.."g")
+end
+SLASH_TRY1 = "/d"
