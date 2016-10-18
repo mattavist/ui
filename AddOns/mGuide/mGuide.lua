f607e3b2e47af88ff1878ad6d7c6899407410db9
Mattavist <matavist@gmail.com>
2016-10-18 07:04:33 -0700

    Slightly better GCD tracking

--- AddOns/mGuide/mGuide.lua
+++ AddOns/mGuide/mGuide.lua
@@ -1,28 +1,17 @@
-local b = CreateFrame("BUTTON", "mGuideFrame", UIParent, "SecureHandlerClickTemplate");
-b:SetSize(50,50)
-b:SetPoint("CENTER",0,-190)
-b:RegisterForClicks("AnyUp")
+local b = CreateFrame("BUTTON", "mGuideFrame", UIParent, "SecureHandlerClickTemplate")
 local mbg = b:CreateTexture(nil, "BACKGROUND")
-mbg:SetPoint("BOTTOMRIGHT", 3, -3)
-mbg:SetPoint("TOPLEFT", -3, 3)
-mbg:SetColorTexture(0, 0, 0)
 local t = b:CreateTexture(nil,"BACKGROUND",nil,1)
-t:SetTexCoord(0.1,0.9,0.1,0.9) --cut out crappy icon border
-t:SetAllPoints(b) --make texture same size as button
-
-CreateFrame("Cooldown", "PlayerCooldown", b, "CooldownFrameTemplate")
-PlayerCooldown:SetAllPoints(b)
-
-
-local lastSpell = nil
+local gcdTime = 0
 
 local function checkSpell(spellName)
 	local canCast = true
 	learned, notEnoughMana = IsUsableSpell(spellName)
 	start, cooldown, enable = GetSpellCooldown(spellName)
 	if start ~= 0 then
-		toReady = GetTime() - start
-		canCast = cooldown - toReady <= 1.6
+		ChatFrame3:AddMessage(gcdTime.."  "..start.."  "..cooldown)
+		--toReady = GetTime() - start
+		--canCast = cooldown - toReady <= 1.25.
+		canCast = gcdTime >= start + cooldown - .1
 	end
 
 	return canCast and not notEnoughMana
@@ -43,53 +32,63 @@ local function checkBuff(buffName)
     return false
 end
 
-local throttleCount = 0
-b:SetScript("OnUpdate", function()
-	--if not InCombatLockdown() then
-	--	return
-	--end
 
-    -- Save CPU by only running once every throttleCount times
-    throttleCount = throttleCount + 1
-    if (throttleCount < 5) then -- Hard coded for performance
-        return
-    end
-    throttleCount = 0
+_, class, _ = UnitClass("player")
+if class == "SHAMAN" then
+	b:SetSize(50,50)
+	b:SetPoint("CENTER",0,-190)
+	mbg:SetPoint("BOTTOMRIGHT", 3, -3)
+	mbg:SetPoint("TOPLEFT", -3, 3)
+	mbg:SetColorTexture(0, 0, 0)
+	t:SetTexCoord(0.1,0.9,0.1,0.9) --cut out crappy icon border
+	t:SetAllPoints(b) --make texture same size as button
+	CreateFrame("Cooldown", "PlayerCooldown", b, "CooldownFrameTemplate")
+	PlayerCooldown:SetAllPoints(b)
 
-	local spell = nil
-	if not checkBuff("Flametongue") then
-		spell = "Flametongue"
-	elseif checkSpell("Stormstrike") then
-		spell = "Stormstrike"
-	elseif checkSpell("Boulderfist") then
-		spell = "Boulderfist"
-	elseif checkSpell("Lava Lash") then
-		spell = "Lava Lash"
-	else
-		spell = "Lightning Bolt"
-	end
+	local throttleCount = 0
+	local lastSpell = nil
+	b:SetScript("OnUpdate", function()
+		--if not InCombatLockdown() then
+		--	return
+		--end
 
-	if not (spell == lastSpell) then
-		setSpell(spell)
-	end
-end)
+	    -- Save CPU by only running once every throttleCount times
+	    throttleCount = throttleCount + 1
+	    if (throttleCount < 5) then -- Hard coded for performance
+	        return
+	    end
+	    throttleCount = 0
 
-b:RegisterForClicks("AnyUp", "AnyDown")
-local upDown = { [false] = "Up", [true] = "Down" }
-local function show(text, color)
-  DEFAULT_CHAT_FRAME:AddMessage(text, color, color, color)
+		local spell = nil
+		if not checkBuff("Flametongue") then
+			spell = "Flametongue"
+		elseif checkSpell("Stormstrike") then
+			spell = "Stormstrike"
+		elseif checkSpell("Boulderfist") then
+			spell = "Boulderfist"
+		--elseif checkSpell("Lava Lash") then
+		--	spell = "Lava Lash"
+		else
+			spell = "Lightning Bolt"
+		end
+
+		if not (spell == lastSpell) then
+			setSpell(spell)
+		end
+	end)
+
+	-- Show GCD Swirl on Frame
+	b:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	b:SetScript("OnEvent", function(self, _, unit, spellName, _)
+		if unit == "player" then
+			time = GetTime()
+			if gcdTime > time + 1.25 then
+				return
+			end
+			gcdTime = time + 1.25
+			ChatFrame3:AddMessage(spellName)
+			PlayerCooldown:SetCooldown(time, 1.25)
+		end
+	end)
 end
-local color = .60
-b:SetScript("OnMouseDown", function(self, button)
-  color = .60
-  show(format("OnMouseDown: %s", button), color, color, color)
-  PlayerCooldown:SetCooldown(GetTime(), 1.5)
-end)
 
-b:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-b:SetScript("OnEvent", function(self, _, unit, spellName, _)
-	if unit == "player" then
-		--ChatFrame3:AddMessage(unit.."  "..spellName)
-		PlayerCooldown:SetCooldown(GetTime(), 1.25)
-	end
-end)
