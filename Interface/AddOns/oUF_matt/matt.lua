c7452cb256a8e0efdb33aec7dd4aaedb65a2ac6f
Mattavist <matavist@gmail.com>
2024-11-27 22:51:08 -0700

    Bar shows up

--- Interface/AddOns/oUF_matt/matt.lua
+++ Interface/AddOns/oUF_matt/matt.lua
@@ -3,19 +3,56 @@ local debug = function(msg)
 end
 
 local cfg = {
-	FrameMargin = {200, 300},
+	-- FrameMargin = {200, 300},
+	FrameMargin = {2, 3},
 	FrameScale = 1.0,
 }
 
-local oUF_matt = {}
-oUF_matt.sharedStyle = function(self, unit, isSingle)
+local UnitSpecific = {
+	target = function(self)
+		local Health = CreateFrame('StatusBar', nil, self)
+		Health:SetHeight(20)
+		Health:SetPoint('TOP')
+		Health:SetPoint('LEFT')
+		Health:SetPoint('RIGHT')
+
+		-- Add a background
+		local Background = Health:CreateTexture(nil, 'BACKGROUND')
+		Background:SetAllPoints()
+		Background:SetTexture(1, 1, 1, .5)
+
+		-- Options
+		Health.colorTapping = true
+		Health.colorDisconnected = true
+		Health.colorClass = true
+		Health.colorReaction = true
+		Health.colorHealth = true
+
+		-- Make the background darker.
+		Background.multiplier = .5
+
+		-- Register it with oUF
+		Health.bg = Background
+		self.Health = Health
+
+	end,
+}
+
+local Shared = function(self, unit)
+	if(UnitSpecific[unit]) then
+		-- Formalize this
+		local unitFrame = UnitSpecific[unit](self)
+		self:SetSize(250, 50)
+		self:SetScale(2.0)
+		return unitFrame
+	end
 end
 
--- Frame creation --------------------------------
+oUF:RegisterStyle("matt", Shared)
+oUF:Factory(function(self)
 
-oUF:RegisterStyle("matt", oUF_matt.sharedStyle)
-oUF:SetActiveStyle("matt")
+     self:SetActiveStyle("matt")
 
-oUF:Spawn("target", "oUF_target"):SetPoint("LEFT", UIParent, "CENTER", cfg.FrameMargin[1], -cfg.FrameMargin[2])
+     self:Spawn("target"):SetPoint("CENTER")
 
-oUF_target:SetScale(cfg.FrameScale)
+end)
